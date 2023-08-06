a374acf52f819e7a3d11654a950ef2846accc4ac
Dheeraj Bisht <dheerajbisht876@gmail.com>
2023-08-06 20:34:58 +0530

    client end for login and onboarding done

--- client/src/pages/_app.jsx
+++ client/src/pages/_app.jsx
@@ -1,5 +1,19 @@
 import "@/styles/globals.css";
 
+import { StateProvider } from "@/context/StateContext";
+import reducer, { initialState } from "@/context/StateReducers";
+import Head from "next/head";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <StateProvider initialState={initialState} reducer={reducer}>
+        <Head>
+          <title>Oshaberi</title>
+          <link rel="shortcut icon" href="/favicon.png" />
+        </Head>
+        <Component {...pageProps} />
+      </StateProvider>
+    </>
+  );
 }
